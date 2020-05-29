a1eaeaf36964c3e64bb7e16ccb38e4e1024a9015
apitoong <apitoong.development@gmail.com>
2020-05-29 15:54:07 +0700

    Initial Commit

---
+++ tesData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tesData
+//
+//  Created by afitra mamor on 29/05/20.
+//  Copyright © 2020 afitra mamor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
